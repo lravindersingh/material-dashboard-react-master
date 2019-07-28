e564d982ecc8149ab739f956525dbc4b417e6795
Ravinder <lravinder@ssdi.sharp.co.in>
2019-07-28 09:47:36 +0530

    Remove brand icon

--- src/layouts/Admin.jsx
+++ src/layouts/Admin.jsx
@@ -34,7 +34,6 @@ import routes from "routes.js";
 import dashboardStyle from "assets/jss/material-dashboard-react/layouts/dashboardStyle.jsx";
 
 import image from "assets/img/sidebar-2.jpg";
-import logo from "assets/img/reactlogo.png";
 
 let ps;
 
@@ -115,8 +114,6 @@ class Dashboard extends React.Component {
       <div className={classes.wrapper}>
         <Sidebar
           routes={routes}
-          logoText={"Creative Tim"}
-          logo={logo}
           image={this.state.image}
           handleDrawerToggle={this.handleDrawerToggle}
           open={this.state.mobileOpen}
